19d10e3d602d5c4b192d5e7197131bc6afa57f90
Suru-Suraj <118275588+Suru-Suraj@users.noreply.github.com>
2023-09-11 18:46:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,3 @@
-provider "aws" {
-  region = "us-east-1"  # Replace with your desired region
-}
 resource "aws_key_pair" "capstone" {
   key_name = "capstone"
   public_key = file("capstone.pem")  # Replace with the path to your public key file
