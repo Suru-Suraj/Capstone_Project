50e45f01662f3e211a5200f2f45152b90722f71b
Suru-Suraj <118275588+Suru-Suraj@users.noreply.github.com>
2023-09-11 18:50:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "capstone" {
   key_name = "capstone"
-  public_key = file("capstone.pem")  # Replace with the path to your public key file
+  public_key = file("capstone.pub")  # Replace with the path to your public key file
 }
