65486e55bb0dbd66efc4c5af446c29e081981d75
Suru-Suraj <118275588+Suru-Suraj@users.noreply.github.com>
2023-09-12 13:21:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,62 @@
-resource "aws_vpc" "capstone_vpc" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "CAPSTONE" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
   tags = {
-    Name = "capstone_vpc"
+    Name = "CAPSTONE"
   }
 }
+resource "aws_subnet" "PUBLIC-1" {
+  vpc_id     = aws_vpc.CAPSTONE.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+}
+resource "aws_subnet" "PUBLIC-2" {
+  vpc_id     = aws_vpc.CAPSTONE.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+}
+resource "aws_subnet" "PRIVATE" {
+  vpc_id     = aws_vpc.CAPSTONE.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1c"
+}
+resource "aws_route_table" "PUBLIC" {
+  vpc_id = aws_vpc.CAPSTONE.id
+}
+resource "aws_route_table" "PRIVATE" {
+  vpc_id = aws_vpc.CAPSTONE.id
+}
+resource "aws_route_table_association" "PUBLIC-1" {
+  subnet_id      = aws_subnet.PUBLIC-1.id
+  route_table_id = aws_route_table.PUBLIC.id
+}
+resource "aws_route_table_association" "PUBLIC-2" {
+  subnet_id      = aws_subnet.PUBLIC-2.id
+  route_table_id = aws_route_table.PUBLIC.id
+}
+resource "aws_route_table_association" "PRIVATE" {
+  subnet_id      = aws_subnet.PRIVATE.id
+  route_table_id = aws_route_table.PRIVATE.id
+}
+resource "aws_internet_gateway" "CAPSTONE" {
+  vpc_id = aws_vpc.CAPSTONE.id
+}
+resource "aws_internet_gateway_attachment" "VPC_ATTACH" {
+  internet_gateway_id = aws_internet_gateway.CAPSTONE.id
+  vpc_id              = aws_vpc.CAPSTONE.id
+}
+resource "aws_route_table_association" "IG-ROUTE" {
+  gateway_id     = aws_internet_gateway.CAPSTONE.id
+  route_table_id = aws_route_table.PUBLIC.id
+}
+resource "aws_eip" "CAPSTONE" {
+  domain   = "vpc"
+}
+resource "aws_nat_gateway" "CAPSTONE" {
+  allocation_id = aws_eip.CAPSTONE.id
+  subnet_id     = aws_subnet.PUBLIC-1.id
+}
+
+
+
