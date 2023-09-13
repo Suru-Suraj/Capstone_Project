83d7b99b21731cee2dd19312acf18a86e4e4377b
suru <surusuraj21@gmail.com>
2023-09-13 17:16:01 +0530

    Thirteen

--- main.tf
+++ main.tf
@@ -137,7 +137,33 @@ resource "aws_security_group" "CAPSTONE" {
 }
 resource "aws_key_pair" "CAPSTONE" {
   key_name = "CAPSTONE"
-  public_key = file("~/.ssh/capstone.pub")
+  public_key = "-----BEGIN RSA PRIVATE KEY-----
+MIIEowIBAAKCAQEAi57NfYZUEVHZ2jFQBrQaQk9MY4tOdqIdkrqil/sgNmrprdja
+DDsDjLO2JjIHQbQMX1mcPIsclti6jOhRr3+xgTHHBYfDZzjAXJ1+NpaUo2eNyt8K
+W7znIn8G4PcbBwawnjsR8KJ3J0c5en/caDOgCQ0/68lp363nM7oSrFOgJtlR5XEG
+2ra8pZXfLnua7Ne1GVE/PbCKnxt/JaQJP8tEt45ID44QMEe5xe7VC2yc3l8AmkZf
+rZKICJrNu9zZo+HGNUl2vB3JnRIhUj7eCbu79tD84uYkR1Iiko7IuLFqjKxEnn/l
+aT97752AwGggrrkqxfDGP6FmpA5AP9h6N4CyVQIDAQABAoIBAH9DCFraDdabnM7d
+E+yITUcTsLUrTSGlMv+DTqDpLbtsSANGHbn4MIwXYnf+Mc8Zpg1jSJ2Gz1BPy/qT
+56Dn64uvK5FG5j035V6Fz31CU8l9oijAlFh6HtO6pTJ5ChGlb2dKwFduPt12dlXK
+JcR1CfI2OYVZ2T8g7BSiDlM1Bk1hdZyKp1XHs1+6r0oxblrX+6MGRDIn1BmKgLUN
+3+58S+AqSsCvbjE1q05gVCXci5JUDYVNQBhvjTApnpdJAz6FkhmEdIJcl+aeXCe3
+YkduRuExMLbN1zYU/lT0V91ZiYdl75W/T7aXJWnJKxAcqTWAvaft8VqNmhzlrOlO
+XK+5aiECgYEA8h2GecDBEHZtCkBJk4g0X/Vn8WhVU+pwcYyQIdQpPzMoqTvFW/le
+vSMKS2hJffqij+gSDy2fu/S/lUlMind7dq3j3/skV/BQbluMH/epS8lwWDwoOd+V
+rx8isS4fj6xiebYnKTMptboRP+C2lnmUuT7Kkjb8XeMjpUehMVXUpRcCgYEAk6CO
+jNNFEd+g5jBTlzsyEGwAuTWteAunNCKhyOxRTnRtqimfSyF3R/nA1kUpSSHt4O/N
+GI7US2A/IUR8ym1UlURPEpflr8i0qMFWPDUKHUfGqivOEm6thLjEhkWeRUpMGBDF
+vEnww7PSlXKIqt8DGw9yngEJvbqksTCrAJ5hX3MCgYAvcFuN9ZeeiyW4UXpZHuHs
+P2Bba670X0Yi6YDAMBrY2ERKTHzSFZ1bf8cfmVJ1DavWeIk5Uh4vLLhxHsiRhPG7
+Aj1ZAJNRa7PGu8dL6DHSuKh7kWXsWQOyKW6ZusjSVNuCr48iwnSUt91zX6tG9pGO
+bmAwn/d4ye2ND+jkisW8hwKBgQCG1SD9bETsYzSgqUjfmnhKWWp7vpP2TaZkTrE3
+Quer4Vj8DtHnm+RG/8xpp3dizTNnu322TVWGlpxyNQxJEnwrbN1PnR5yY7zlWNf7
+W3ImdhTz/dhUK3QeAWe1P0akAIzpegAvzdSx2b6fMqGydsduYriLqWUNPbgIAexd
+P0quEwKBgBx7V65yiTedg0wq73AefQ/ty9agbImB48epk75eYswt1o+2ip+TjGcV
+fQOs07Bu3CDEt3yEkIzGyNbhhEquqhyNf+OG8FA/M2WG49hf1MjmNY0qE0Ws9afk
+tp/DlBVcINunMrogQEXc0fEXBsHLR5VKNzlLAHPc/Zh5GaBbv6gP
+-----END RSA PRIVATE KEY-----"
 }
 resource "aws_instance" "CAPSTONE-PUBLIC" {
   ami           = "ami-053b0d53c279acc90"
