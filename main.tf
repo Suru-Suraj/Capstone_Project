b05fa0a32e1b90a94e5b872bd28928e02a99d96c
suru <surusuraj21@gmail.com>
2023-09-13 08:23:27 +0530

    First

--- main.tf
+++ main.tf
@@ -10,22 +10,41 @@ resource "aws_subnet" "PUBLIC-1" {
   vpc_id     = aws_vpc.CAPSTONE.id
   cidr_block = "10.0.1.0/24"
   availability_zone = "us-east-1a"
+  tags = {
+    Name = "PUBLIC-1"
+  }
 }
 resource "aws_subnet" "PUBLIC-2" {
   vpc_id     = aws_vpc.CAPSTONE.id
   cidr_block = "10.0.2.0/24"
   availability_zone = "us-east-1b"
+  tags = {
+    Name = "PUBLIC-2"
+  }
 }
 resource "aws_subnet" "PRIVATE" {
   vpc_id     = aws_vpc.CAPSTONE.id
   cidr_block = "10.0.3.0/24"
   availability_zone = "us-east-1c"
+  tags = {
+    Name = "PRIVATE"
+  }
 }
 resource "aws_route_table" "PUBLIC" {
   vpc_id = aws_vpc.CAPSTONE.id
+  tags = {
+    Name = "PUBLIC"
+  }
 }
 resource "aws_route_table" "PRIVATE" {
   vpc_id = aws_vpc.CAPSTONE.id
+    route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.CAPSTONE.id
+  }
+  tags = {
+    Name = "PRIVATE"
+  }
 }
 resource "aws_route_table_association" "PUBLIC-1" {
   subnet_id      = aws_subnet.PUBLIC-1.id
@@ -41,6 +60,9 @@ resource "aws_route_table_association" "PRIVATE" {
 }
 resource "aws_internet_gateway" "CAPSTONE" {
   vpc_id = aws_vpc.CAPSTONE.id
+  tags = {
+    Name = "CAPSTONE"
+  }
 }
 resource "aws_route_table_association" "IG-ROUTE" {
   gateway_id     = aws_internet_gateway.CAPSTONE.id
@@ -48,11 +70,67 @@ resource "aws_route_table_association" "IG-ROUTE" {
 }
 resource "aws_eip" "CAPSTONE" {
   domain   = "vpc"
+  tags = {
+    Name = "CAPSTONE"
+  }
 }
 resource "aws_nat_gateway" "CAPSTONE" {
   allocation_id = aws_eip.CAPSTONE.id
   subnet_id     = aws_subnet.PUBLIC-1.id
+  tags = {
+    Name = "CAPSTONE"
+  }
 }
 
+resource "aws_security_group" "CAPSTONE" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.CAPSTONE.id
 
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.CAPSTONE.cidr_block]
+  }
 
+    ingress {
+    description      = "TLS from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.CAPSTONE.cidr_block]
+  }
+
+    ingress {
+    description      = "TLS from VPC"
+    from_port        = 3000
+    to_port          = 3000
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.CAPSTONE.cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "CAPSTONE"
+  }
+}
+
+resource "aws_instance" "CAPSTONE" {
+  ami           = "ami-053b0d53c279acc90"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.CAPSTONE.id]
+  subnet_id = aws_subnet.PRIVATE.id
+
+  tags = {
+    Name = "CAPSTONE"
+  }
+}
