85ff5aa1eb5984b3daa364571900f2f377e73e7c
Suru-Suraj <118275588+Suru-Suraj@users.noreply.github.com>
2023-09-15 10:04:34 +0530

    Update main(ami).tf

--- 2.AMI/main(ami).tf
+++ 2.AMI/main(ami).tf
@@ -1,5 +1,4 @@
-variable "source_instance_id" {
-  description = "The ID of the source AWS EC2 instance from which to create the AMI."
-  type        = string
-  default     = ""  # Provide a default value or remove it if you want to specify it at runtime
+resource "aws_ami_from_instance" "CAPSTONE" {
+  name               = "CAPSTONE"
+  source_instance_id = var.source_instance_id
 }
