da20c7f4cd6346e808d82266b8155a5b1a50e0f2
Suru-Suraj <118275588+Suru-Suraj@users.noreply.github.com>
2023-09-15 10:00:37 +0530

    Update main.tf

--- 1.Terraform_and_Ansible/main.tf
+++ 1.Terraform_and_Ansible/main.tf
@@ -132,17 +132,3 @@ resource "aws_instance" "CAPSTONE" {
     Name = "CAPSTONE-PUBLIC"
   }
 }
-
-# Output values
-output "private_ip" {
-  value = aws_instance.CAPSTONE.private_ip
-}
-
-output "public_ip" {
-  value = aws_instance.CAPSTONE.public_ip
-}
-
-output "instance_id" {
-  description = "Instance ID of the EC2 instance"
-  value       = aws_instance.CAPSTONE.id
-}
