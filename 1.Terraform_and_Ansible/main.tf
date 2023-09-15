07a12c5d356cc2990d64fb1c82cea791d04e79f2
Suru-Suraj <118275588+Suru-Suraj@users.noreply.github.com>
2023-09-15 09:58:49 +0530

    Update main.tf

--- 1.Terraform_and_Ansible/main.tf
+++ 1.Terraform_and_Ansible/main.tf
@@ -1,5 +1,6 @@
+# Create the VPC
 resource "aws_vpc" "CAPSTONE" {
-  cidr_block       = "10.0.0.0/16"
+  cidr_block       = var.vpc_cidr_block
   instance_tenancy = "default"
 
   tags = {
@@ -7,41 +8,50 @@ resource "aws_vpc" "CAPSTONE" {
   }
 }
 
+# Create the public subnet
 resource "aws_subnet" "PUBLIC" {
   vpc_id     = aws_vpc.CAPSTONE.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
+  cidr_block = var.subnet_cidr_block
+  availability_zone = var.subnet_availability_zone
   map_public_ip_on_launch = true
+
   tags = {
     Name = "PUBLIC-1"
   }
 }
 
+# Create the route table for the public subnet
 resource "aws_route_table" "PUBLIC" {
   vpc_id = aws_vpc.CAPSTONE.id
+
   tags = {
     Name = "PUBLIC"
   }
 }
 
+# Associate the route table with the public subnet
 resource "aws_route_table_association" "PUBLIC" {
   subnet_id      = aws_subnet.PUBLIC.id
   route_table_id = aws_route_table.PUBLIC.id
 }
 
+# Create an internet gateway
 resource "aws_internet_gateway" "CAPSTONE" {
   vpc_id = aws_vpc.CAPSTONE.id
+
   tags = {
     Name = "CAPSTONE"
   }
 }
 
+# Create a default route to the internet via the internet gateway
 resource "aws_route" "CAPSTONE" {
   route_table_id         = aws_route_table.PUBLIC.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.CAPSTONE.id
 }
 
+# Create a security group
 resource "aws_security_group" "CAPSTONE" {
   name        = "CAPSTONE"
   description = "Allow TLS inbound traffic"
@@ -92,30 +102,38 @@ resource "aws_security_group" "CAPSTONE" {
   }
 }
 
+# Create an EC2 key pair
 resource "aws_key_pair" "capstone" {
-  key_name   = "capstone"
+  key_name   = var.instance_key_name
   public_key = tls_private_key.capstone.public_key_openssh
 }
+
+# Create a TLS private key
 resource "tls_private_key" "capstone" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
+
+# Create a local file containing the private key
 resource "local_file" "capstone" {
   content  = tls_private_key.capstone.private_key_pem
   filename = "capstone.pem"
 }
 
+# Create an EC2 instance
 resource "aws_instance" "CAPSTONE" {
-  ami           = "ami-053b0d53c279acc90"
-  instance_type = "t2.micro"
+  ami           = var.instance_ami
+  instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.CAPSTONE.id]
-  subnet_id = aws_subnet.PUBLIC.id
-  key_name = "capstone"
+  subnet_id     = aws_subnet.PUBLIC.id
+  key_name      = var.instance_key_name
+
   tags = {
     Name = "CAPSTONE-PUBLIC"
   }
 }
 
+# Output values
 output "private_ip" {
   value = aws_instance.CAPSTONE.private_ip
 }
@@ -123,6 +141,7 @@ output "private_ip" {
 output "public_ip" {
   value = aws_instance.CAPSTONE.public_ip
 }
+
 output "instance_id" {
   description = "Instance ID of the EC2 instance"
   value       = aws_instance.CAPSTONE.id
