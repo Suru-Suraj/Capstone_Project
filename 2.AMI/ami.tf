69b3ced16fc9688f4a8a06c7ef26055829d32e73
suru <surusuraj21@gmail.com>
2023-09-15 07:38:14 +0530

    71

--- 2.AMI/ami.tf
+++ 2.AMI/ami.tf
@@ -3,5 +3,5 @@ resource "aws_ami_from_instance" "CAPSTONE" {
   source_instance_id = ""
 }
 output "ami_id" {
-  value = aws_from_instance.CAPSTONE.ami
+  value = aws_ami_from_instance.CAPSTONE.arn
 }
